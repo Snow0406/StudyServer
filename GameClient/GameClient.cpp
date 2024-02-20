9f78bf4f89f248e662ddb3dc5424fb5277f127da
Snow <87596507+Snow0406@users.noreply.github.com>
2024-02-20 22:09:37 +0900

    ud

--- GameClient/GameClient.cpp
+++ GameClient/GameClient.cpp
@@ -11,7 +11,7 @@ using namespace std;
 int main()
 {
     Sleep(1000);
-    
+
     printf("============ CLIENT ===========\n");
 
     WORD wVersionRequested;
@@ -33,35 +33,83 @@ int main()
         return 1;
     }
 
+    u_long iMode = 1;
+    if (ioctlsocket(connectSocket, FIONBIO, &iMode) == INVALID_SOCKET)
+    {
+        printf("ioctlsocket function failed %d\n", WSAGetLastError());
+        closesocket(connectSocket);
+        WSACleanup();
+        return 1;
+    }
+
     SOCKADDR_IN service; //서버의 SOCKADDR_IN
     memset(&service, 0, sizeof(service));
     service.sin_family = AF_INET;
     inet_pton(AF_INET, "127.0.0.1", &service.sin_addr);
     service.sin_port = htons(7777);
 
-    if (connect(connectSocket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR)
-    {
-        printf("connect failed %d\n", WSAGetLastError());
-        closesocket(connectSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    printf("connected.");
-
     while (true)
     {
-        char sendBuffer[] = "Hello This is Client hahahaha !";
-        if (send(connectSocket, sendBuffer, sizeof(sendBuffer), 0) == SOCKET_ERROR)
+        if (connect(connectSocket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR)
         {
-            printf("Send Failed %d\n", WSAGetLastError());
+            //non-blocking 연결 시도 중 발생하는 일반적인 오류 처리
+            if (WSAGetLastError() == WSAEWOULDBLOCK || WSAGetLastError() == WSAEALREADY)
+            {
+                continue;
+            }
+
+            //연결이 완료되었는지 확인
+            if (WSAGetLastError() == WSAEISCONN)
+            {
+                break;
+            }
+
+            //연결 실패 시 오류 메세지 출력
+            printf("connect failed with error %d\n", WSAGetLastError());
             closesocket(connectSocket);
             WSACleanup();
             return 1;
         }
-        
+    }
+    printf("connected.\n");
+    char sendBuffer[] = "Hello This is Client hahahaha !";
+
+    //데이터 전송할 준비
+    WSAEVENT wsaEvent = WSACreateEvent();	//winsock 이벤트 객체를 생성
+    WSAOVERLAPPED overlapped = {};			//비동기 I/O 작업을 위한 구조체 초기화
+    overlapped.hEvent = wsaEvent;			//overlapped 구조체에 이벤트 객체를 할
+
+    while (true)
+    {
+        WSABUF wsaBuf;						//WSABUF 구조체 선언. winsock 함수에 사용될 버퍼를 관리
+        wsaBuf.buf = sendBuffer;			//버퍼 포인터를 sendBuffer로 설정
+        wsaBuf.len = sizeof(sendBuffer);	//버퍼의 길이 설정
+
+        DWORD sendLen = 0;					//전송된 데이터의 길이를 저장할 변수
+        DWORD flags = 0;					//전송 시 사용할 추가 옵션 플래그. 현재는 사용 안함
+
+        //WSASend 함수를 사용하여 데이터를 비동기적으로 전송
+        if (WSASend(connectSocket, &wsaBuf, 1, &sendLen, flags, &overlapped, nullptr) == SOCKET_ERROR)
+        {
+            //WSA_IO_PENDING 에러는 비동기 작업이 대기 중임을 의미
+            if (WSAGetLastError() == WSA_IO_PENDING)
+            {
+                //비동기 작업이 완료될 때까지 대기
+                WSAWaitForMultipleEvents(1, &wsaEvent, TRUE, WSA_INFINITE, FALSE);
+                //비동기 작업의 결과를 가져옴. 여기서 실제로 데이터가 전송
+                WSAGetOverlappedResult(connectSocket, &overlapped, &sendLen, FALSE, &flags);
+            }
+            else //진짜 오류
+            {
+                break;
+            }
+        }
+        printf("Send Buffer Lebgth : %d bytes\n", sendLen);
+
         Sleep(1000);
     }
+
+    WSACloseEvent(wsaEvent);
     closesocket(connectSocket);
     WSACleanup();
 }
--- StudyServer/StudyServer.cpp
+++ StudyServer/StudyServer.cpp
@@ -108,7 +108,7 @@ int main()
         //lpFlages : 플래그
         //lpOverlapped : WSAOVERLAPPED 구조체 대한 포인터
         //lpCompletionRoutine : 수신 완료 후 호출되는 콜백함수
-        
+
         WSAOVERLAPPED overrlapped = {};
         WSAEVENT wsaEvent = WSACreateEvent(); //비동기 작업을
         overrlapped.hEvent = wsaEvent; //WSAOVERLAPPED 구조체에 있는 hEvent에 객체 연결
@@ -118,24 +118,36 @@ int main()
         while (true)
         {
             WSABUF wsaBuf; //WSABUF 구조체 선언, WSARecv 함수에 사용될 버퍼를 관리
-wsaBuf.buf = recvBuffer; //버퍼 포인터를 recvBuffer로 설정
+            wsaBuf.buf = recvBuffer; //버퍼 포인터를 recvBuffer로 설정
             wsaBuf.len = sizeof(recvBuffer); //버퍼 크기 설정
 
-            DWORD recvLen = 0;
+            DWORD recvLen = 0; //수신된 데이터 길이를 저장할 변수
             DWORD flags = 0; //플래그 변수, 현재는 사용하지 않음
 
-            if (WSARecv(acceptSocket, &wsaBuf, 1, &recvLen, &flags, &overrlapped, nullptr)) //WSARecv 함수를 아용해 비동기적으로 데이터를 수신 요청
+            //WSARecv 함수를 이용해 비동기적으로 데이터를 수신 요청
+            if (WSARecv(acceptSocket, &wsaBuf, 1, &recvLen, &flags, &overrlapped, nullptr))
+            //WSARecv 함수를 아용해 비동기적으로 데이터를 수신 요청
             {
+                //Recv 작업이 성공적으로 시작되었으며 나중에 완료될 경우
                 if (WSAGetLastError() == WSA_IO_PENDING)
                 {
+                    //스신 작업이 완료될때까지 이벤트 대기
                     WSAWaitForMultipleEvents(1, &wsaEvent, TRUE, WSA_INFINITE, FALSE);
                     //완료된 비동기 작업을 가져옴. 이 과정에서 실제로 데이터가 수신됨
                     WSAGetOverlappedResult(acceptSocket, &overrlapped, &recvLen, FALSE, &flags);
                 }
+                else //SOCKET_ERROR인 상태에서 WSA_IO_PENDING이 아니라면 진짜 에러
+                {
+                    //수신 루프 빠져 나가기
+                    break;
+                }
             }
+
+            printf("Recv Data : %s\n", recvBuffer);
             
         }
-        
+        closesocket(acceptSocket);	//해당 소켓은 닫아주고
+        WSACloseEvent(wsaEvent);	// 사용된 이벤트 객체를 닫음
     }
     closesocket(listenSocket);
     WSACleanup();
